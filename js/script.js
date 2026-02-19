194c386ae62b4b6e290c4979404da688dc66f87e
zi9a <dhanduziya@gmail.com>
2026-02-19 11:59:48 +0530

    few navbar addition

--- js/script.js
+++ js/script.js
@@ -5,58 +5,16 @@ let color = {
 
   changeBackgroundColor: function(color) {
     switch (color) {
-      case 'darkblue':
-        this.backgroundColor = 'darkblue';
-        break;
-
-      case 'black':
-        this.backgroundColor = 'black';
-        break;
-
-      case 'darkgreen':
-        this.backgroundColor = 'darkgreen';
-        break;
-
-      case 'purple':
-        this.backgroundColor = 'purple';
-        break;
-
-      case 'brown':
-        this.backgroundColor = 'brown';
-        break;
-
-      case 'darkcyan':
-        this.backgroundColor = 'darkcyan';
-        break;
-
-      case 'darkslategray':
-        this.backgroundColor = 'darkslategray';
-        break;
-
-      default:
-        this.backgroundColor = 'crimson';
-        break;
+      case 'darkblue': this.backgroundColor = 'darkblue'; break;
+      case 'black': this.backgroundColor = 'black'; break;
+      case 'darkgreen': this.backgroundColor = 'darkgreen'; break;
+      case 'purple': this.backgroundColor = 'purple'; break;
+      case 'brown': this.backgroundColor = 'brown'; break;
+      case 'darkcyan': this.backgroundColor = 'darkcyan'; break;
+      case 'darkslategray': this.backgroundColor = 'darkslategray'; break;
+      default: this.backgroundColor = 'crimson'; break;
     }
   }
 }
+const button = document.querySelector("button");
 
-// little tests
-async function foo() {
-  let colors = [
-    'darkcyan',
-    'darkblue',
-    'black',
-    'darkgreen',
-    'brown',
-    'crimson',
-    'darkslategray',
-    'purple',
-  ]
-
-  setInterval(() => {
-    colors.forEach(element => {
-      document.body.style.backgroundColor = element;
-    }, 5000);
-  });
-}
-foo()
