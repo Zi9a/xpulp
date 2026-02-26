9dc06f273764b8ec9d143c3d8dc96e2d1f575022
zi9a <dhanduziya@gmail.com>
2026-02-26 23:37:33 +0530

    footer updated

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let pervious_scroll_position = window.pageYOffset;
+window.onscroll = function() {
+  let current_scroll_position = window.pageYOffset;
+  if (current_scroll_position < pervious_scroll_position) {
+    document.querySelector('header').style.top = "0";
+  } else if (current_scroll_position > pervious_scroll_position) {
+    document.querySelector('header').style.top = "-10%";
+  }
+  pervious_scroll_position = current_scroll_position;
+}
